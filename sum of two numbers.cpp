7d4e9bdcc9badc8a6e58c4f6ff9346e36a33710d
cr13-cr <73814087+cr13-cr@users.noreply.github.com>
2021-03-25 13:07:52 +0530

    Create sum of two numbers.cpp

---
+++ sum of two numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int x,y,sum;
+  cout<<"\n ENTER THE VALUE OF X AND Y : ";
+  cin>>x>>y;
+  sum=x+y;
+  cout<<"\n THE SUM OF X AND Y IS = "<<sum;
+  return 0;
+}
